43e5d2ba126b680a2dec0bb658d445a305ef7a68
Marcus Carvalho <marcuswac@gmail.com>
2020-12-11 10:56:02 -0300

    Updated estimates

--- bin/update-ne.sh
+++ bin/update-ne.sh
@@ -13,21 +13,18 @@ do
   STATE_DIR=$STATES_DIR/$state
   
   ##Update data
-  #mv $STATE_DIR/data/cases/latest.csv $STATE_DIR/data/cases/previous.csv
-  #Rscript R/update-data-state.R $state $STATES_DIR latest
+  mv $STATE_DIR/data/cases/latest.csv $STATE_DIR/data/cases/previous.csv
+  Rscript R/update-data-state.R $state $STATES_DIR latest
 
   ##Update Rt estimates
-  #rm -rf $STATE_DIR/data/rt/previous 
-  #mv $STATE_DIR/data/rt/latest $STATE_DIR/data/rt/previous
-  #Rscript R/update-rt-state.R $state $STATES_DIR latest
+  rm -rf $STATE_DIR/data/rt/previous 
+  mv $STATE_DIR/data/rt/latest $STATE_DIR/data/rt/previous
+  Rscript R/update-rt-state.R $state $STATES_DIR latest
 
   ##Update report
-  #mkdir -p $STATE_DIR/docs
   mkdir -p $STATE_DIR
-  #cp -r docs/index.Rmd docs/header.html docs/footer.html $STATE_DIR/docs/
   cp -r docs/index.Rmd docs/header.html docs/footer.html $STATE_DIR
   cp -r data/shapefile $STATE_DIR/data/
   touch $STATE_DIR/.here
-  #Rscript -e "rmarkdown::render('$STATE_DIR/docs/index.Rmd')"
   Rscript -e "rmarkdown::render('$STATE_DIR/index.Rmd')"
 done
